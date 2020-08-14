670d3429204d634408987853b389b9b221ad047b
Mary Aiyetigbo <aiyetigbodamilola@gmail.com>
2020-08-14 09:44:49 +0100

    First Commit

---
+++ src/FibonacciSequence.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class FibonacciSequence {
+    public static void main (String args[]) {
+        int n1 = 0;
+        int n2 = 1;
+        int n3;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Kindly supply the sequence count: ");
+        int n = sc.nextInt();
+        System.out.print(n1 +" "+ n2);
+
+        for (int i=0; i<n; i++)
+        {
+            n3=n1+n2;
+            System.out.print(" "+n3);
+
+            n1=n2;
+            n2=n3;
+        }
+
+    }
+}
